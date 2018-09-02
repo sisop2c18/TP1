a7b3fa0d028df76913e768fe274506d73ddc118a
Micaela Ramos <ramosmicaela@outlook.es>
2018-09-02 00:19:21 -0300

    Revert "Ejercicios 1 y 4"
    This reverts commit 206a208153e0f779945123d0cacbd630eae6e335.

--- Ejercicio1/Ejercicio1.ps1
+++
@@ -1,8 +0,0 @@
-1- El objetivo del script es escribir en un archivo los primeros n procesos (siendo n=3 o el parámetro cantidad
-que se pase) con formato Id Nombre en un archivo de texto.
-
-2- Agregaría como validación del parámetro cantidad, que no sea mayor a la cantidad de procesos en ejecución.
-
-3- Si el script se ejecuta sin parámetros y el archivo por defecto no existe la dirección mostrará por pantalla 
-"El path no existe". De otra forma, si el archivo existe, el script se ejecutará tomando como cantidad de procesos 3
-y el archivo a escribir será procesos.txt y funcionará sin inconvenientes.
