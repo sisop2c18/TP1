3a4615ecb22eac158307bd7504e3546242693953
Nicolas Sapaya <nicolas.sapaya@gmail.com>
2018-10-03 17:47:44 +0000

    Fix Ej6: re entrega 2

--- Ejercicio 6/Ejercicio6.ps1
+++ Ejercicio 6/Ejercicio6.ps1
@@ -1,41 +1,80 @@
 ﻿<# 
-.Synopsis
-    INTEGRANTES:
-                - Schafer Federico
-                - Sapaya Nicolas
-                - Secchi Lucas
-                - Ramos Micaela
-                - Miller Lucas
 .DESCRIPTION
     A partir de un archivo .zip pasado por parámetro indica la relación de compresión de cada uno de los archivos que contiene.
 
 .EXAMPLE
     .\Ejercicio6.ps1 -pathZip Test.zip
+	
+.NOTES
+Nombre del Script: Ejercicio6.ps1
+Trabajo Práctico Nro. 1 - Ejercicio 6
+Integrantes:
+            Miller, Lucas            39353278
+            Ramos, Micaela           39266928
+            Schafer, Federico        39336856
+            Sapaya, Nicolás Martín   38319489
+            Secchi, Lucas            39267345
+Instancia de Entrega: Segunda Re Entrega
 #>
 
-Param
-(
-    [parameter(Mandatory = $True, position = 1)][ValidateNotNullOrEmpty()][string] $pathZip
+Param(
+		[parameter(Mandatory = $True, position = 1)][ValidateNotNullOrEmpty()][string] $pathZip
 )
 
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-    $primero = 0
-    $RawFiles = [System.IO.Compression.ZipFile]::OpenRead($(Join-path $pwd $pathZip)).Entries
-    foreach($RawFile in $RawFiles) {
+function saveTable{
+    <#
+    .SYNOPSIS
+        Guarda la tabla y la muestra.
+         
+    .DESCRIPTION
+        Se encarga de guardar en una tabla todos los archivos del zip y mostrarla por pantalla.
+    
+    .PARAMETER RawFiles
+		Todos los archivos adentro del zip.
+    #>
+
+    Param([Parameter(Mandatory = $true)]$RawFiles
+    )
+
+    #Creo Arrays donde almaceno.
+    $arrayNombre = @()
+    $arrayTComp = @()
+	$arrayTReal = @()
+    $arrayRelacion = @()
+
+    #Creo una tabla.
+    $tableNew = New-Object system.Data.DataTable
+
+    #Defino columnas.
+    $colString = New-Object system.Data.DataColumn Nombre,([string])
+    $colString1 = New-Object system.Data.DataColumn TamañoComprimido,([string])
+	$colString2 = New-Object system.Data.DataColumn TamañoOriginal,([string])
+	$colString3 = New-Object system.Data.DataColumn Relacion,([string])
+
+    #Agrego columnas.
+    $tableNew.columns.add($colString)
+    $tableNew.columns.add($colString1)
+	$tableNew.columns.add($colString2)
+	$tableNew.columns.add($colString3)
+	
+	foreach($RawFile in $RawFiles) {
         $var = [String]$RawFile
         $var = $var.Substring($var.length-1)
         if($var -ne "/"){
-            $object = New-Object -TypeName PSObject            
-            $Object | Add-Member -MemberType NoteProperty -Name NombreArchivo -Value $RawFile.Name                        
-            $Object | Add-Member -MemberType NoteProperty -Name TamañoComprimido -Value ($RawFile.CompressedLength/1MB).ToString("0.000")           
-            $Object | Add-Member -MemberType NoteProperty -Name TamañoOriginal -Value ($RawFile.Length/1MB).ToString("0.000")
-            $Object | Add-Member -MemberType NoteProperty -Name Relacion -Value (($RawFile.CompressedLength).Tostring("00")/($RawFile.Length)).Tostring("0.000")
-            if($primero -eq 0){
-                $Object | Format-Table -AutoSize
-                $primero = 1
-            }
-            else{
-                $Object | Format-Table -AutoSize -HideTableHeaders
-                }
-        }
-    }
+			$arrayNombre += $RawFile.Name
+			$arrayTComp += ([math]::Round($($RawFile.CompressedLength/1MB),4)).ToString()
+			$arrayTReal += ([math]::Round($($RawFile.Length/1MB),4)).ToString()
+			$arrayRelacion += ([math]::Round((($RawFile.CompressedLength)/($RawFile.Length)),4)).ToString()
+		}
+	}
+	
+	for($i=0;$i -lt $arrayNombre.length;$i++){
+		$tableNew.Rows.Add($arrayNombre[$i],$arrayTComp[$i],$arrayTReal[$i],$arrayRelacion[$i]) >$null 2>&1
+	}
+	
+	$tableNew | Format-Table -AutoSize
+}
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+$RawFiles = [System.IO.Compression.ZipFile]::OpenRead($(Join-path $pwd $pathZip)).Entries
+saveTable -RawFiles $RawFiles
